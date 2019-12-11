249c9c7ee4d09b049f6a3544bad4242271935f2c
pgavriluk <pgavriluk@gmail.com>
2019-12-11 11:32:53 -0500

    first commit

---
+++ simple-modules/hello.modules/com/pavel/modules/hello/HelloModules.java
@@ -0,0 +1,7 @@
+package com.pavel.modules.hello;
+ 
+public class HelloModules {
+    public static void doSomething() {
+        System.out.println("Hello, Modules!");
+    }
+}
---
+++ simple-modules/main.app/com/pavel/modules/main/MainApp.java
@@ -0,0 +1,9 @@
+package com.pavel.modules.main;
+ 
+import com.pavel.modules.hello.HelloModules;
+ 
+public class MainApp {
+    public static void main(String[] args) {
+        HelloModules.doSomething();
+    }
+}
